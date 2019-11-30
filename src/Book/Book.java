8d4e914dcb48a2361a7cc7755a54fc7fa6bbacd1
Shankha Pal <shankhp@amazon.com>
2019-11-30 14:33:55 -0800

    Exploring how to write Simple APIs locally, through Java and Tomcat Server

---
+++ src/Book/Book.java
@@ -0,0 +1,38 @@
+package Book;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@Path("/book")
+public class Book {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayHelloXML() {
+		String response = "<?xml version='1.0'?>" +
+				"<hello>Hello XML</hello>";
+		return response;
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayHelloHTML() {
+		String response = "<h1>Hello HTML</h1>";
+		return response;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String sayHelloJSON() {
+		String response = "Hello JSON";
+		return response;
+	}
+	
+
+}
